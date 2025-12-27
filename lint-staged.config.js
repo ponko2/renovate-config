3b32b47e12beeae4535b0d0c5f7f9f08aa6bd4da
Takahito Nakano <kano@ponko2.jp>
2025-12-28 02:39:27 +0900

    chore: Migrate git hooks manager to Lefthook

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * @filename: lint-staged.config.js
- * @type {import('lint-staged').Configuration}
- */
-export default {
-  "*.{cjs,css,js,json,jsx,mjs,ts,tsx,yaml,yml}": "prettier --write",
-  "default.json": "renovate-config-validator --strict",
-};
